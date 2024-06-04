2d5a2f47ba63e7c3c3bd75bd097de4592c8d06e8
hoshino1212 <arupakaman@gmail.com>
2024-06-04 12:46:23 +0900

    登録、ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root 'home#index'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  post 'users', to: 'users#create'
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new', as: :login
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: :logout
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_03_054159) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_04_031459) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
     t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
